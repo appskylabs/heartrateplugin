a89d9bfa1c527e3856a6a099ceba58bc807ea89a
Taylor Korensky <taylor@appskylabs.com>
2017-06-18 23:20:41 -0500

    updating the JS file in the plugin to match the fucntion name in iOS

--- www/plugin.js
+++ www/plugin.js
@@ -5,7 +5,7 @@ var PLUGIN_NAME = 'HeartRatePlugin';
 
 var HeartRatePlugin = {
   pluginInitialize: function(cb) {
-    exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
+    exec(cb, null, PLUGIN_NAME, 'pluginInitialize', []);
   },
 };
 
